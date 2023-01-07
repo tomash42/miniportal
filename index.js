beef79c0ef307675a1847a44357c56b1564d755b
tomek <tomash40@yandex.com>
2023-01-07 16:55:35 +0100

    1

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const flash = require('express-flash');
 const sesion = require('express-session');
 const passport = require('passport');
 const initializePassport = require("./passConf");
-const registryUser = require('./registryUser')
+
 
 
 
@@ -83,7 +83,59 @@ app.get('/panel',checkNotAuthenticated,(req,res)=>{
 /* post */
 
 /*registry new user and check if already exist user  */
-app.post('/',registryUser)
+app.post('/',async (req,res)=>
+{  
+try{
+    /* 
+    rejestracja
+    1 - sprawdzam czy wystepuje login 
+      1a -jesli nie to tworze konto
+    */
+let {name,surname, login, password, tel,email,address} = req.body;
+
+  hashPass = await bcrypt.hash(password, 10);
+
+  // Validation passed
+  pool.query(
+    `SELECT * FROM account
+      WHERE userlogin = $1`,
+    [login],
+    (err, results) => {
+      if (err) {
+        console.log(err);
+      }
+      console.log(results.rows);
+
+      if (results.rows.length > 0) {
+        console.log(results.rows);
+        const userLogin = req.body;
+        req.flash("bad_msg", ` ${userLogin.login} juz istnieje`);
+        res.redirect("/")
+        
+      } else {
+        //create account
+        pool.query(
+          `insert into account(username, usersurname,userlogin,userpassword,usertel,usermail,useraddress)
+                values('${name}','${surname}','${login}','${hashPass}',${tel},'${email}','${address}')`,
+          (err, results) => {
+            if (err) {
+              throw err;
+            }
+            console.log(results.rows);
+            const userLogin = req.body;
+            req.flash("success_msg", `Witaj ${userLogin.login} . Możesz się zalogować`);
+            res.redirect("/");
+          }
+        );
+      }
+    }
+  );
+}
+catch{
+console.log("err")
+res.redirect('/')
+}
+})
 
 /* send question */
 app.post('/question',(req,res)=>{
