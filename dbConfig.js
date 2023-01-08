afcc5ddf98ec250ff9cad4506fd94ba7107ad6ed
tomek <tomash40@yandex.com>
2023-01-08 08:20:18 +0100

    q

--- dbConfig.js
+++ dbConfig.js
@@ -5,7 +5,7 @@ set conf postgresql
 require("dotenv").config();
 const {Pool} = require("pg");
 const isProd = process.env.NODE_ENV === "production";
-const connetion = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`; 
+const connetion = `postgres://wcabirzl:h4-XMEJbz84Jbis-iAwGrjkffL7ok6yk@snuffleupagus.db.elephantsql.com/wcabirzl`; 
 const  pool = new Pool(
     {
         connectionString: isProd ? process.env.DB_DATABASE_URL : connetion 
